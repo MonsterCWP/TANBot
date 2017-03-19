c18f11836d2bb5c08ace990f688146ede4d455ee
Bill Pugh <pugh@cs.umd.edu>
2017-03-19 18:41:57 -0400

    Number of changes to GUI

--- Trig Project/src/TriangleBotGUI.java
+++ Trig Project/src/TriangleBotGUI.java
@@ -4,13 +4,15 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class TriangleBotGUI  implements ActionListener {
+public class TriangleBotGUI implements ActionListener {
     final static boolean shouldFill = true;
     final static boolean shouldWeightX = true;
     JTextField a, b, c, A, B, C;
@@ -22,7 +24,7 @@ public class TriangleBotGUI  implements ActionListener {
         c.gridx++;
         c.insets = new Insets(0, 0, 0, 0);
         c.weightx = 1;
-        JTextField field = new JTextField();
+        JTextField field = new JFormattedTextField(DecimalFormat.getNumberInstance());
         field.setColumns(4);
         pane.add(field, c);
         c.gridx++;
@@ -33,20 +35,21 @@ public class TriangleBotGUI  implements ActionListener {
 
         pane.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-       constraints.gridx = 0;
+        constraints.gridx = 0;
         constraints.gridy = 0;
-        
-//        constraints.fill = GridBagConstraints.HORIZONTAL;
-//        JTextArea description = new JTextArea("Now is the time for all good men to come to the aid of their country");
-//        description.setLineWrap(true);
-//        description.setWrapStyleWord(true);
-//        constraints.insets = new Insets(5,5,5,5);
-//        constraints.gridwidth = 4;
-//        pane.add(description, constraints);
-//        constraints.gridwidth = 1;
-//        constraints.fill  = GridBagConstraints.NONE;
-//        constraints.gridy++;
-                
+
+        // constraints.fill = GridBagConstraints.HORIZONTAL;
+        // JTextArea description = new JTextArea("Now is the time for all good
+        // men to come to the aid of their country");
+        // description.setLineWrap(true);
+        // description.setWrapStyleWord(true);
+        // constraints.insets = new Insets(5,5,5,5);
+        // constraints.gridwidth = 4;
+        // pane.add(description, constraints);
+        // constraints.gridwidth = 1;
+        // constraints.fill = GridBagConstraints.NONE;
+        // constraints.gridy++;
+
         a = addField(pane, constraints, "a");
         A = addField(pane, constraints, "A");
 
@@ -63,11 +66,25 @@ public class TriangleBotGUI  implements ActionListener {
         constraints.gridx = 0;
         constraints.gridy++;
         constraints.gridwidth = 4;
-        JButton b = new JButton("Calculate");
-        pane.add(b, constraints);
-        b.addActionListener(this);
+        JButton calculate = new JButton("Calculate");
+        pane.add(calculate, constraints);
+        calculate.addActionListener(this);
+        constraints.gridx = 0;
+        constraints.gridy++;
+        constraints.gridwidth = 4;
+        JButton clear = new JButton("Clear");
+        pane.add(clear, constraints);
+        clear.addActionListener(e -> {
+            a.setText("");
+            A.setText("");
+            b.setText("");
+            B.setText("");
+            c.setText("");
+            C.setText("");
+        });
 
     }
+  
 
     private void createAndShowGUI() {
         // Create and set up the window.
@@ -94,32 +111,64 @@ public class TriangleBotGUI  implements ActionListener {
 
     public static double getDouble(JTextField field) {
         String txt = field.getText();
-        if (txt.isEmpty()) return Double.NaN;
+        if (txt.isEmpty())
+            return Double.NaN;
         return Double.parseDouble(txt);
     }
-    
+
     public static String format(double d) {
+        if (Double.isNaN(d))
+            return "";
         if (d == (long) d)
-            return String.format("%d", (long)d);
-        return String.format("%.1f",  d);
+            return String.format("%d", (long) d);
+        return String.format("%.1f", d);
     }
-    
+
     public static void set(JTextField angle, JTextField side, Side x) {
-        angle.setText(format( x.angle()));
+        angle.setText(format(x.angle()));
         side.setText(format(x.side()));
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Side sideA = new Side(getDouble(a), getDouble(A), "a");
         Side sideB = new Side(getDouble(b), getDouble(B), "b");
         Side sideC = new Side(getDouble(c), getDouble(C), "c");
-        
+
         Trig.solve(sideA, sideB, sideC);
-        
+
         set(A, a, sideA);
         set(B, b, sideB);
         set(C, c, sideC);
-       
-        
+
+        double cx = Trig.cos(sideA.angle()) * sideB.side();
+        double cy = Trig.sin(sideA.angle()) * sideB.side();
+        double ax = 0;
+        double ay = 0;
+        double bx = sideC.side();
+        double by = 0;
+
+        if (cx < 0) {
+            ax += -cx;
+            bx += -cx;
+            cx = 0.0;
+        }
+
+        showTriangle(ax, ay, bx, by, cx, cy);
     }
+
+    private void showTriangle(double ax, double ay, double bx, double by, double cx, double cy) {
+        // Create and set up the window.
+        JFrame frame = new JFrame("Resulting triangle");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Set up the content pane.
+        frame.getContentPane().add(new TrianglePanel(ax, ay, bx, by, cx, cy), null);
+
+        // Display the window.
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
 }
---
+++ Trig Project/src/TrianglePanel.java
@@ -0,0 +1,85 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
+
+import javax.swing.JPanel;
+
+public class TrianglePanel extends JPanel {
+    private static final int SIZE = 600;
+    private static final int BORDER = 10;
+    double ax, ay, bx, by, cx, cy;
+    double centerX, centerY;
+    Path2D myPath;
+
+    static void drawString(Graphics2D g2, String s, double x, double y) {
+        FontMetrics fm = g2.getFontMetrics();
+
+        g2.drawString(s, (float) x - fm.stringWidth(s) / 2, (float) y - fm.getHeight() / 2 + fm.getAscent());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.draw(myPath);
+
+        FontMetrics fm = g2.getFontMetrics();
+
+        drawString(g2, "A", (5 * ax + centerX) / 6, (5 * ay + centerY) / 6);
+        drawString(g2, "B", (5 * bx + centerX) / 6, (5 * by + centerY) / 6);
+        drawString(g2, "C", (5 * cx + centerX) / 6, (5 * cy + centerY) / 6);
+        
+        
+        drawString(g2, "a", (bx+cx)/2 - (ax-centerX)/8, (by+cy)/2 - (ay-centerY)/8);
+        drawString(g2, "b", (ax+cx)/2 - (bx-centerX)/8, (ay+cy)/2 - (by-centerY)/8);
+        
+        drawString(g2, "c", (bx+ax)/2 - (cx-centerX)/8, (by+ay)/2 - (cy-centerY)/8);
+        
+        
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        if (isPreferredSizeSet()) {
+            return super.getPreferredSize();
+        }
+        double height = ay - (cy-centerY)/8 + 2*BORDER;
+        
+        return new Dimension((int)(Double.max(bx, cx) + 2 * BORDER), (int)height );
+    }
+
+    double max(double... values) {
+        double result = Double.MIN_VALUE;
+        for (double d : values)
+            if (result < d)
+                result = d;
+        return result;
+    }
+
+    public TrianglePanel(double ax, double ay, double bx, double by, double cx, double cy) {
+        super();
+        setBackground(Color.WHITE);
+        double scale = SIZE / max(ax, ay, bx, by, cx, cy);
+
+        this.ax = ax * scale + BORDER;
+        this.ay = cy * scale + BORDER;
+        this.bx = bx * scale + BORDER;
+        this.by = cy * scale + BORDER;
+        this.cx = cx * scale + BORDER;
+        this.cy =  BORDER;
+        this.centerX = (this.ax+this.bx+this.cx)/3;
+        this.centerY = (this.ay+this.by+this.cy)/3;
+        
+        
+        myPath = new Path2D.Double();
+        myPath.moveTo(this.ax, this.ay);
+        myPath.lineTo(this.bx, this.by);
+        myPath.lineTo(this.cx, this.cy);
+        myPath.closePath();
+    }
+
+}
